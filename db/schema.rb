dbc4a9dc1c8678e5e78981a218e595acf60e6c5b
Vitalii Yulieff <wwjurew@gmail.com>
2024-10-03 10:53:42 +0000

    add article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_03_104753) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_105310) do
+  create_table "articles", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nickname"
     t.datetime "created_at", null: false
